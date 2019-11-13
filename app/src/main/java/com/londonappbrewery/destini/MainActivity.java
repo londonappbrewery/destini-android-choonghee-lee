b6b896af5eb925f00aa7d26ed8ea91fed3366fb2
Choonghee Lee <choonghee.lee@hotmail.com>
2019-11-13 11:25:24 +0900

    Step 3: Create the Variables for the Views

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -1,11 +1,15 @@
 package com.londonappbrewery.destini;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
+    TextView story;
+    Button top, bottom;
 
 
     @Override
